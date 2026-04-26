7580195a15b720a02f252b3fb79ab3c9dc7b1404
ahmedgouda5 <ahmedgodayyy@gmail.com>
2026-04-27 01:15:21 +0300

    inital for Analytics branch

--- src/(Pages)/Reports.tsx
+++ src/(Pages)/Reports.tsx
@@ -1,7 +1,7 @@
 const Reports = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Reports</h1>
+      <h1 className="text-2xl font-bold">hello this page for reports</h1>
     </div>
   );
 };
