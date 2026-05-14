4977151656cadda65647c8a06eff2b0d36c4f0cc
ahmedgouda5 <ahmedgodayyy@gmail.com>
2026-05-14 12:52:13 +0300

    chore: remove unused file configurations

--- src/components/Ai_chat/Hooks/useCaht.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { ContextChat } from "../Context/ContextChat";
-export const useChat = () => {
-  const context = useContext(ContextChat);
-  if (!context) {
-    throw new Error("useChat must be used within ContextProvider");
-  }
-  return context;
-};
